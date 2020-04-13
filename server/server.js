b7255ddd9053c3a1e4e6447e4999c34033f26361
Mojtaba Esmaili <mj.esm.1999@gmail.com>
2020-04-13 19:26:52 +0430

    Create User By REST Module

--- server/server.js
+++ server/server.js
@@ -3,17 +3,43 @@
 //-------config direction
 process.env.NODE_CONFIG_DIR = __dirname + '/config';
 const config = require('config');
+const express = require('express');
+const _ = require('loadsh');
 
 const {User} = require('./model/user');
 
 console.log(`*** ${String(config.get('level')).toUpperCase()} ***`);
 
-let newUser = new User({
-    fullName:'James Milner',
-    email: 'Tsfafdrn.com',
-    password:'123456'
+// let newUser = new User({
+//     fullName:'James Milner',
+//     email: 'Tsfafdrn.com',
+//     password:'123456'
+// });
+
+// newUser.save().then((user)=>{
+//     console.log(user);
+// });
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/api/users',(req,res)=>{
+    const body = _.pick(req.body,['fullName','email', 'password']);
+
+    console.log(body);
+
+    let user = new User(body);
+
+    user.save().then((user)=>{
+        res.status(200).send(user);
+    },(err)=>{
+        res.status(400).json({
+            Error: `Something went wrong ${err}`
+        });
+    })
 });
 
-newUser.save().then((user)=>{
-    console.log(user);
+app.listen(config.get('PORT'),()=>{
+    console.log(`Server is running at port ${config.get('PORT')}`);
 });
